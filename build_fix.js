05c095770288526fc7a0836f2163c51d52c12787
vutran1710 <me@vutr.io>
2021-08-08 17:06:34 +0700

    up

--- build_fix.js
+++ build_fix.js
@@ -1,10 +1,5 @@
 var shell = require('shelljs')
 
-if (!shell.which('git')) {
-  shell.echo('Sorry, this script requires git')
-  shell.exit(1)
-}
-
 shell.cp('-R', 'build/static/js/*', 'build/')
 shell.cp('-R', 'build/static/css/*', 'build/')
 shell.rm('-rf', 'build/static/')
